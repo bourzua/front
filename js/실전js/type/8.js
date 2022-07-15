0c9cfc8f4bea1140d21af608c6e7ad932813a4b3
jua <bourzua@naver.com>
2022-07-15 14:49:58 +0900

    기본타입

---
+++ js/실전js/type/8.js
@@ -0,0 +1,13 @@
+// 등호 3개를 추천
+
+console.log(123 === 123);
+console.log('123' === '123');
+console.log('123' === 123);
+console.log(0 === false);
+console.log(123 === true);
+console.log('-----')
+console.log(123 == 123);
+console.log('123' == '123');
+console.log('123' == 123);
+console.log(0 == false);
+console.log(123 == true);
--- js/실전js/type/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-oString.call([]));
